29bd618d846bbbfdaea9a1a33654bf7ac6bf730a
Gergely Nagy <gnanet@users.noreply.github.com>
2026-03-19 17:48:46 +0100

    Create SearchAPI class with full-text search, advanced search, archive search, and statistics retrieval methods.

---
+++ src/API/SearchAPI.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace API;
+
+class SearchAPI
+{
+    public function fullTextSearch($query) {
+        // Implementation of full-text search using ZincSearch
+    }
+
+    public function advancedSearch($filters) {
+        // Implementation of advanced search using ZincSearch
+    }
+
+    public function archiveSearch($archiveId) {
+        // Implementation of archive search using ZincSearch
+    }
+
+    public function getStatistics() {
+        // Implementation of statistics retrieval from ZincSearch
+    }
+}
